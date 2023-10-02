d9feaf5beea30b3522bec5a1398770ac745555da
Ingri109 <146620280+Ingri109@users.noreply.github.com>
2023-10-02 19:13:35 +0200

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -43,7 +43,20 @@ open.addEventListener('click', function () {
     menu.addEventListener('click', function () {
         menu.classList.add('hidden');
     })
-
-
 })
 
+
+//Плавний перехід
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
